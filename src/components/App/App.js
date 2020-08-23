003266e3cc60caf31403832a12440406f7713e5a
Kameron Ahler <kamahler@gmail.com>
2020-08-23 17:14:53 -0600

    refine view

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,26 +1,48 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 
 export default function App() {
   const [resourceType, setResourceType] = useState('posts')
   const [currentData, setCurrentData] = useState([])
 
-  useEffect( () => {
+  useEffect(() => {
     fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
-      .then(response => response.json())
-      .then(json => setCurrentData(json))
+      .then((response) => response.json())
+      .then((json) => setCurrentData(json))
   }, [resourceType])
 
   return (
-    <div>
-      <button onClick={() => {setResourceType('posts')}}>Posts</button>
-      <button onClick={() => {setResourceType('users')}}>Users</button>
-      <button onClick={() => {setResourceType('comments')}}>Comments</button>
-      <h1>{resourceType}</h1>
+    <section>
+      <header>
+        <h1>{resourceType}</h1>
+      </header>
       <div>
-        {currentData.map(el => {
-          return <pre key={el.id}>{JSON.stringify(el)}</pre>
-        })}
+        <button
+          onClick={() => {
+            setResourceType('posts')
+          }}
+        >
+          Posts
+        </button>
+        <button
+          onClick={() => {
+            setResourceType('users')
+          }}
+        >
+          Users
+        </button>
+        <button
+          onClick={() => {
+            setResourceType('comments')
+          }}
+        >
+          Comments
+        </button>
       </div>
-    </div>
-  );
+      <ul>
+        {currentData.map((el) => {
+          return <li key={el.id}>{JSON.stringify(el.id)}</li>
+        })}
+      </ul>
+    </section>
+  )
 }
