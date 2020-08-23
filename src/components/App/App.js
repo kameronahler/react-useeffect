3e582c364365a506d10bc740ca844ceb8bdbbd3b
Kameron Ahler <kamahler@gmail.com>
2020-08-23 17:00:08 -0600

    add dummy json

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function App() {
   const [resourceType, setResourceType] = useState('posts')
+
+  useEffect( () => {
+    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+      .then(response => response.json())
+      .then(json => console.log(json))
+  }, [resourceType])
+
   return (
     <div>
       <button onClick={() => {setResourceType('posts')}}>Posts</button>
