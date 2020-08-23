007f16134034b60813c51b6133a1d8dccdc8aabd
Kameron Ahler <kamahler@gmail.com>
2020-08-23 16:53:53 -0600

    add exercise basics

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function App() {
+  const [resourceType, setResourceType] = useState('posts')
   return (
     <div>
+      <button onClick={() => {setResourceType('posts')}}>Posts</button>
+      <button onClick={() => {setResourceType('users')}}>Users</button>
+      <button onClick={() => {setResourceType('comments')}}>Comments</button>
+      <h1>{resourceType}</h1>
     </div>
   );
 }
