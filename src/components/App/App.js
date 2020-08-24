f61393ecbc66c359d7deaf58bca7b2e8c1b5c591
Kameron Ahler <kamahler@gmail.com>
2020-08-23 18:04:24 -0600

    separate json from new excercise

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,27 +1,6 @@
-import React, { useState, useEffect } from 'react'
-import Header from '../Header/Header'
-import ButtonSet from '../ButtonSet/ButtonSet'
-import DisplayList from '../DisplayList/DisplayList'
+import React from 'react'
+import SectionJSON from '../SectionJSON/SectionJSON'
 
 export default function App() {
-  const [category, setCategory] = useState('posts')
-  const [currentData, setCurrentData] = useState([])
-
-  useEffect(() => {
-    fetch(`https://jsonplaceholder.typicode.com/${category}`)
-      .then((response) => response.json())
-      .then((json) => setCurrentData(json))
-  }, [category])
-
-  const buttonSetsCategory = (e) => {
-    setCategory(e.currentTarget.textContent)
-  }
-
-  return (
-    <section>
-      <Header category={category} />
-      <ButtonSet action={buttonSetsCategory} />
-      <DisplayList currentData={currentData} />
-    </section>
-  )
+  return <SectionJSON />
 }
---
+++ src/components/SectionJSON/SectionJSON.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react'
+import Header from '../Header/Header'
+import ButtonSet from '../ButtonSet/ButtonSet'
+import DisplayList from '../DisplayList/DisplayList'
+
+export default function SectionJSON() {
+  const [category, setCategory] = useState('posts')
+  const [currentData, setCurrentData] = useState([])
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${category}`)
+      .then((response) => response.json())
+      .then((json) => setCurrentData(json))
+  }, [category])
+
+  const buttonSetsCategory = (e) => {
+    setCategory(e.currentTarget.textContent)
+  }
+
+  return (
+    <section>
+      <Header category={category} />
+      <ButtonSet action={buttonSetsCategory} />
+      <DisplayList currentData={currentData} />
+    </section>
+  )
+}
