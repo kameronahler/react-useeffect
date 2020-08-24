85b0853dc4d382e84474aa4600e428e645c28841
Kameron Ahler <kamahler@gmail.com>
2020-08-23 18:06:16 -0600

    create width section

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import SectionJSON from '../SectionJSON/SectionJSON'
+import SectionWidth from '../SectionWidth/SectionWidth'
 
 export default function App() {
-  return <SectionJSON />
+  return (
+    <>
+      <SectionWidth />
+      <SectionJSON />
+    </>
+  )
 }
